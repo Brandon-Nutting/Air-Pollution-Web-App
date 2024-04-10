6be5b715528e6bb04839b72189d97deb2c90ed97
Brandons1998 <bran.swett@gmail.com>
2024-04-09 20:29:37 -0400

    Correcting an error

--- pollution_app.py
+++ pollution_app.py
@@ -10,36 +10,40 @@ pollution_df = pd.read_csv("Input/Air_Quality.csv")
 stylesheet = ['https://codepen.io/chriddyp/pen/bWLegP.css']
 app = Dash(_name_, external_stylesheets=stylesheet)
 
-# Determines the layout of the app
-# Displays three dropdowns as one row
-app.layout = html.Div([
-   html.Div(
-       html.H1("Analysis of air pollution",
-               style={"textAlign":"center"}),
-       className="row"),
-   html.Div(
-       dcc.Graph(id = 'Pollution Chart', figure=fig),
-       className="row"),
-   html.Div(
-       dcc.Dropdown(
-           id='Filter Dropdown',
-           multi=True,
-           options=[
-               {"label" : x, "value": x}
-               for x in sorted(pollution_df['Geo Place Name'].unique())]
-           value = [x for x in pollution_df['Geo Place Name'].unique()],
+app.layout = html.Div(
+    [
+        html.Div(
+            html.H1(
+                "Analysis of air pollution data", style={"textAlign": "center"}
+            ),
+            className="row",
         ),
-       className="three columns",
-   ),
-   html.Div(
-       html.A(
-           id="Data Link",
-           children="Click on link to view source data",
-           href="https://www.kaggle.com/datasets/sahirmaharajj/air-pollution-dataset?resource=download",
-           target="_blank",
-           style = {"color" : "blue", "fontSize" : "40px"}
-       ),
-       className="two columns",
-   ),
-], className="row", # className accepts classes from stylesheets.
-), 
+        html.Div(dcc.Graph(id="pollution-chart", figure={}), className="row"),
+        html.Div(
+            [
+                html.Div(
+                    dcc.Dropdown(
+                        id="filter-dropdown",
+                        multi=True,
+                        options=[
+                            {"label": x, "value": x}
+                            for x in sorted(pollution_df['Geo Place Name'].unique())
+                        ],
+                        value=[x for x in pollution_df['Geo Place Name'].unique()],
+                    ),
+                    className="three columns",
+                ),
+                html.Div(
+                    html.A(
+                        id="my-link",
+                        children="Click here to view the source data",
+                        href="https://www.kaggle.com/datasets/sahirmaharajj/air-pollution-dataset?resource=download",
+                        target="_blank",
+                    ),
+                    className="two columns",
+                ),
+            ],
+            className="row",
+        ),
+    ]
+)
