3eaabf0134d717193675dfc04553cd0d82774557
Brandons1998 <bran.swett@gmail.com>
2024-05-20 17:15:10 -0400

    Another push

--- Advanced Application Demo.py
+++ Advanced Application Demo.py
@@ -168,7 +168,5 @@ def update_graph(n_clicks, stored_dataframe, years_chosen, indct_chosen):
         )
         return fig
     
-dcc.Interval(id = "timer", interval = 1000 * 10, n_intervals=0),
-
 if __name__ == "__main__":
     app.run_server(debug = True)
